496329cc1c9ff313ea16b35749b616106e0deceb
mirzaAfnanBaig <mirzaafnanbaig98@gmail.com>
2023-11-04 00:56:05 +0530

    Assignment 4 Q5 done

--- Assignment_04/Assignment_4_Q5/src/com/sunbeam/List.java
+++ Assignment_04/Assignment_4_Q5/src/com/sunbeam/List.java
@@ -1,25 +1,29 @@
 package com.sunbeam;
 
-
 public class List {
-	public class Node{
+	int counter;
+
+	public class Node {
 		private int data;
 		private Node next;
+
 		public Node(int value) {
 			// TODO Auto-generated constructor stub
 			data = value;
 			next = null;
 		}
 	}
-	
+
 	private Node head;
+
 	public List() {
 		head = null;
 	}
+
 	public boolean isEmpty() {
 		return head == null;
 	}
-	
+
 	public void addFirst(int value) {
 		Node newnode = new Node(value);
 		if (isEmpty())
@@ -27,6 +31,7 @@ public class List {
 		else
 			newnode.next = head;
 		head = newnode;
+		counter++;
 	}
 
 	public void addLast(int value) {
@@ -39,13 +44,15 @@ public class List {
 				trav = trav.next;
 			trav.next = newnode;
 		}
+		counter++;
 	}
-	
+
 	public void deleteFirst() {
 		if (isEmpty())
 			System.out.println("List is empty");
 		else
 			head = head.next;
+		counter--;
 	}
 
 	public void deleteLast() {
@@ -60,41 +67,76 @@ public class List {
 				trav = trav.next;
 			trav.next = null;
 		}
+		counter--;
 	}
 
+//	public void addAfter2(int value, int pos) {
+//		Node newnode = new Node(value);
+//		if (isEmpty())
+//			head = newnode;
+//		else {
+//			Node trav = head;
+//			for (int i = 1; i < pos; i++)
+//				trav = trav.next;
+//			Node temp = trav.next;
+//			trav.next = newnode;
+//			newnode.next = temp;
+//		}
+//		counter++;
+//	}  // for given position add element 
 	
-	
-	
-	public void addAfter(int value,int pos) {
+	public void addAfter(int posElement, int value) {
 		Node newnode = new Node(value);
 		if(isEmpty())
 			head = newnode;
 		else {
 			Node trav = head;
-			for(int i = 1; i < pos; i++)
+			do {
+				if(trav.data == posElement) {
+					newnode.next = trav.next;
+					trav.next = newnode;
+				}
+				
 				trav = trav.next;
-			Node temp = trav.next;
-			trav.next = newnode;
-			newnode.next = temp;	
+					
+			}while(trav.next != null);
+		}
+		counter++;
+	}
+
+//	public void addBefore(int value, int pos) {
+//		Node newnode = new Node(value);
+//		if (isEmpty())
+//			head = newnode;
+//		else {
+//			Node trav = head;
+//			for (int i = 1; i < pos - 1; i++)
+//				trav = trav.next;
+//			Node temp = trav.next;
+//			trav.next = newnode;
+//			newnode.next = temp;
+//
+//		}
+//		counter++;
+//	}  add before position by given position index
+	public void addBefore(int posvalue,int value) {
+		Node newnode = new Node(value);
+		if(isEmpty())
+			head = newnode;
+		else {
+			Node trav = head;
+			do {
+				if(trav.next.data == posvalue) {
+					Node temp = trav.next;
+					trav.next = newnode;					
+					newnode.next = temp;
+				}
+				trav = trav.next;
+			}while(trav.next != null);
 		}
 		
 	}
-	public void addBefore(int value,int pos) {
-		Node newnode = new Node(value);
-		if(isEmpty())
-			head = newnode;
-		else {
-			Node trav = head;
-			for(int i = 1; i< pos-1; i++)
-				trav = trav.next;
-			Node temp = trav.next;
-			trav.next = newnode;
-			newnode.next = temp;	
-			
-		}
-	}
-	
-	
+
 	public void displayAll() {
 		if (isEmpty())
 			System.out.println("List is empty");
@@ -108,6 +150,26 @@ public class List {
 			} while (trav != null);
 			System.out.println("");
 		}
+	}
+
+	public void sortLinkedList() {
+	
+		Node trav = head;
+		for (; trav.next.next != null; trav = trav.next) {
+			Node trav2 = head;
+			for (; trav2.next != null; trav2 = trav2.next) {
+				if (trav2.data > trav2.next.data) {
+					int temp = trav2.data;
+					trav2.data = trav2.next.data;
+					trav2.next.data = temp;
+				}
+
+			}
+			if (trav2.next == null)
+				trav2 = head;
+
+		}
+
 	}
 
 }
--- Assignment_04/Assignment_4_Q5/src/com/tester/Program.java
+++ Assignment_04/Assignment_4_Q5/src/com/tester/Program.java
@@ -5,15 +5,25 @@ import com.sunbeam.List;
 public class Program {
 	public static void main(String[] args) {
 		List l1 = new List();
-		l1.addFirst(10);
-		l1.addFirst(20);
-		l1.addFirst(30);
-		l1.addFirst(40);
-		l1.addFirst(50);
+		l1.addFirst(5);
+		l1.addFirst(4);
+		l1.addFirst(6);
+		l1.addFirst(9);
+		l1.addFirst(8);
 		
-//		l1.addAfter(66, 4);
-		l1.addBefore(45, 3);
 		l1.displayAll();
+		
+		
+		l1.addAfter(6,20);
+		
+		l1.addBefore(9,140);
+		
+		l1.displayAll();
+		
+		// List: 8 9 6 4 5 
+		// List: 8 140 9 6 20 4 5 
+
+		
 	}
 
 }
